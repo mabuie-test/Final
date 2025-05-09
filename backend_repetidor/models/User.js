bb6725b0b7a4f9df58cd7ed7f88d34248221d95d
mabuie-test <jorgemaabuie@gmail.com>
2025-05-10 01:52:08 +0200

    Update User.js

--- backend_repetidor/models/User.js
+++ backend_repetidor/models/User.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
-});
-
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+  username: String,
+  password: String,
+  role: { type: String, default: 'user' } // 'admin' ou 'user'
 });
 
 module.exports = mongoose.model('User', userSchema);
