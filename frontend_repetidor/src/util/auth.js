bdd663bc53371151f32fa1478f12b62b8736a7be
mabuie-test <jorgemaabuie@gmail.com>
2025-05-09 16:25:38 +0200

    Update auth.js

--- frontend_repetidor/src/util/auth.js
+++ frontend_repetidor/src/util/auth.js
@@ -1,1 +1,20 @@
-hr7g
+import jwtDecode from 'jwt-decode';
+
+export function isAuthenticated() {
+  return !!localStorage.getItem('token');
+}
+
+export function setToken(token) {
+  localStorage.setItem('token', token);
+}
+
+export function getUserRole() {
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  const decoded = jwtDecode(token);
+  return decoded.role;
+}
+
+export function logout() {
+  localStorage.removeItem('token');
+}
