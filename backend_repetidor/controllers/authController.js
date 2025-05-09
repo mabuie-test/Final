81955df19e6893c78090a2e737ee92bdc740ffac
mabuie-test <jorgemaabuie@gmail.com>
2025-05-09 17:03:46 +0200

    Create authController.js

---
+++ backend_repetidor/controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(400).json({ message: 'Usuário não encontrado' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Senha incorreta' });
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ message: 'Erro no servidor' });
+  }
+};
