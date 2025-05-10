85b6aac04091881c168cca97e5e79adcd3342756
mabuie-test <jorgemaabuie@gmail.com>
2025-05-10 02:22:53 +0200

    Update api.js

--- frontend_repetidor/src/services/api.js
+++ frontend_repetidor/src/services/api.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+export const setToken = (token) => {
+  api.defaults.headers.common['Authorization'] = token;
+};
 
 export default api;
