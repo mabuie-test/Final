99dc74141725e7729a6fc0a200b12fc1652a59ea
mabuie-test <jorgemaabuie@gmail.com>
2025-05-11 01:41:53 +0200

    Update logRoutes.js

--- backend_repetidor/routes/logRoutes.js
+++ backend_repetidor/routes/logRoutes.js
@@ -1,9 +1,14 @@
+// routes/logRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const logController = require('../controllers/logController');
 const auth = require('../middleware/authMiddleware');
 
-router.get('/', auth, logController.getLogs);
+// Cria um novo log de repetidor
 router.post('/', auth, logController.createLog);
+
+// Retorna todos os logs (do mais novo para o mais antigo)
+router.get('/', auth, logController.getLogs);
 
 module.exports = router;
