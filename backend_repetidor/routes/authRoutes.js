2ec18293aebe8ca81b88a9ee971ce0d6335569a0
mabuie-test <jorgemaabuie@gmail.com>
2025-05-11 00:51:31 +0200

    Update authRoutes.js

--- backend_repetidor/routes/authRoutes.js
+++ backend_repetidor/routes/authRoutes.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const auth = require('../middleware/authMiddleware');
-const onlyAdmin = require('../middleware/roleMiddleware');
+ const express = require('express');
+ const router = express.Router();
+ const authController = require('../controllers/authController');
+-const auth = require('../middleware/authMiddleware');
+-const onlyAdmin = require('../middleware/roleMiddleware');
++// const auth = require('../middleware/authMiddleware');
+// const onlyAdmin = require('../middleware/roleMiddleware');
 
-// Login e registro
-router.post('/login', authController.login);
-router.post('/register', auth, onlyAdmin('admin'), authController.register);
+ // Login e registro
+ router.post('/login', authController.login);
+-router.post('/register', auth, onlyAdmin('admin'), authController.register);
++// Rota de registro livre, sem autenticação
++router.post('/register', authController.register);
 
-module.exports = router;
+ module.exports = router;
