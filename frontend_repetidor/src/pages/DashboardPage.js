dd905a12fc5dbafa7c82196fd9635c4ba27f4157
mabuie-test <jorgemaabuie@gmail.com>
2025-05-10 13:24:12 +0200

    Add files via upload

---
+++ frontend_repetidor/src/pages/DashboardPage.js
@@ -0,0 +1,12 @@
+import RepeaterList from '../components/RepeaterList';
+import MapView from '../components/MapView';
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Painel de Repetidores</h1>
+      <RepeaterList />
+      <MapView />
+    </div>
+  );
+}
