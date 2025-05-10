8d6abfa3ae60d086eecdf1b1bc66ff5e6d0d571b
mabuie-test <jorgemaabuie@gmail.com>
2025-05-10 02:03:58 +0200

    Update authRoutes.js

--- backend_repetidor/routes/authRoutes.js
+++ backend_repetidor/routes/authRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const auth = require('../middleware/authMiddleware');
+const onlyAdmin = require('../middleware/roleMiddleware');
 
+// Login e registro
 router.post('/login', authController.login);
+router.post('/register', auth, onlyAdmin('admin'), authController.register);
 
 module.exports = router;
