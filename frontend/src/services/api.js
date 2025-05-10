b724ba8c75bbe69dad13a05aabed101d4d43fcb1
mabuie-test <jorgemaabuie@gmail.com>
2025-05-10 12:39:31 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  withCredentials: true,
 });
+
+export const setToken = (token) => {
+  api.defaults.headers.common['Authorization'] = token;
+};
 
 export default api;
