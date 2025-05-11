9d728b9b741b6b4a5d45bd4ce3647148e848b418
mabuie-test <jorgemaabuie@gmail.com>
2025-05-11 16:05:01 +0200

    Update userRoutes.js

--- backend_repetidor/routes/userRoutes.js
+++ backend_repetidor/routes/userRoutes.js
@@ -1,9 +1,16 @@
+// routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+// Criar novo usuário (somente admin)
 router.post('/', authMiddleware(['admin']), userController.createUser);
+
+// Listar todos os usuários (somente admin)
 router.get('/', authMiddleware(['admin']), userController.getUsers);
+
+// Deletar usuário por ID (somente admin)
+router.delete('/:id', authMiddleware(['admin']), userController.deleteUser);
 
 module.exports = router;
