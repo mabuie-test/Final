8f0fabf4fc04914ac4f9c52de4d052954edb8c6b
mabuie-test <jorgemaabuie@gmail.com>
2025-05-11 11:51:15 +0200

    Update api.js

--- frontend_repetidor/src/services/api.js
+++ frontend_repetidor/src/services/api.js
@@ -1,11 +1,13 @@
+// src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL,  // ex: https://seu-backend.onrender.com/api
 });
 
+// Agora prepend “Bearer ”
 export const setToken = (token) => {
-  api.defaults.headers.common['Authorization'] = token;
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
 
 export default api;
