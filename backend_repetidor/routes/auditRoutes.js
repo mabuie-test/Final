66c01d694afd408e19548b10ce4f9d3d273f3f15
mabuie-test <jorgemaabuie@gmail.com>
2025-05-11 22:17:21 +0200

    Update auditRoutes.js

--- backend_repetidor/routes/auditRoutes.js
+++ backend_repetidor/routes/auditRoutes.js
@@ -1,13 +1,24 @@
 // routes/auditRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const Audit = require('../models/Audit');
 const auth = require('../middleware/authMiddleware');
 const onlyAdmin = require('../middleware/roleMiddleware');
 
-router.get('/', auth, onlyAdmin('admin'), async (req, res) => {
-  const logs = await Audit.find().sort({ timestamp: -1 });
-  res.json(logs);
-});
+router.get(
+  '/',
+  auth,
+  onlyAdmin('admin'),
+  async (req, res) => {
+    try {
+      const logs = await Audit.find().sort({ timestamp: -1 });
+      res.json(logs);
+    } catch (err) {
+      console.error('Erro em auditRoutes:', err);
+      res.status(500).json({ message: 'Erro ao buscar histórico' });
+    }
+  }
+);
 
 module.exports = router;
