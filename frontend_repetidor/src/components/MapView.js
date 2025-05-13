25aa3bb24a4f42e551c930743beef12b0e0ae189
mabuie-test <jorgemaabuie@gmail.com>
2025-05-13 16:54:08 +0200

    Update MapView.js

--- frontend_repetidor/src/components/MapView.js
+++ frontend_repetidor/src/components/MapView.js
@@ -1,12 +1,27 @@
+// src/components/MapView.js
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { useEffect, useState } from 'react';
 import api from '../services/api';
 
+// 1) Importar as imagens
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+// 2) Ajustar o Default Icon
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize:     [25, 41],
+  iconAnchor:   [12, 41],
+  popupAnchor:  [1, -34],
+  shadowSize:   [41, 41],
+  shadowAnchor: [12, 41],
 });
 
 export default function MapView() {
@@ -21,8 +36,8 @@ export default function MapView() {
   return (
     <MapContainer center={[-25.96, 32.58]} zoom={6} style={{ height: '300px' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {repeaters.map((r, idx) => (
-        <Marker key={r._id || idx} position={[r.latitude, r.longitude]}>
+      {repeaters.map((r) => (
+        <Marker key={r._id} position={[r.latitude, r.longitude]}>
           <Popup>
             <strong>{r.deviceId}</strong><br />
             Status: {r.status}
